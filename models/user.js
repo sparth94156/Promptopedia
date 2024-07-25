8d1c50a6cd913b25df49bda9b3055a9b2170cd55
Parthsinghpal <sparth94156@gmail.com>
2024-07-25 19:37:33 +0530

    Implemented Tag, prompt Search feature

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
         type: String,
         required: [true, 'Username is required!'],
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, 
-        "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+        "Username invalid, it should contain 8-20 alphanumeric letters and should be unique"]
     },
     image: {
         type: String, 
